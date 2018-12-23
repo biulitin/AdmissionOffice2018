d84fc2ff59475153c4c28eab8848e5a45d96efbf
thepieceofdream <37545725+thepieceofdream@users.noreply.github.com>
2018-12-23 20:47:54 +0300

    Update Script.sql

--- Script.sql
+++ Script.sql
@@ -50,3 +50,76 @@ create table Abiturient (
 	foreign key (id_returnReason) references ReturnReasons(id) on update cascade
 );
 
+-- Тип паспорта 
+create table TypePassport (
+	id int primary key,    --код
+	name text,             --наименование (тип паспорта)
+	codeFIS text           --код выгрузки в ФИС
+);
+
+--Абитуриент_паспорт
+create table AbiturientPassport (
+	series text,           --серия
+	number text,           --номер
+	issued_by text,        --кем выдан
+	dateOf_issue date,     --дата выдачи
+	id_abiturient int,     --код абитуриента
+	id_typePassport int,   --код паспорта
+	
+	--Внешние ключи
+	foreign key (id_abiturient) references Abiturient(aid) on update cascade,
+	foreign key (id_typePassport) references TypePassport(id) on update cascade	
+);
+
+--Регион
+create table Region (
+    id int primary key,  --код
+	name text,			 --наименование
+	codeFIS text		 --код выгрузки в ФИС
+);
+
+--Тип населенного пункта
+create table TypeSettlement (
+    id int primary key,  --код
+	name text,			 --наименование
+	codeFIS text		 --код выгрузки в ФИС
+);
+
+--Абитуриент_адрес 
+create table AbiturientAdress (
+    postcode text,           --индекс
+	adress text,             --адрес
+	id_abiturient int,       --код абитуриента
+	id_region int,           --код региона
+	id_typeSettlement int,   --код населенного пункта
+	
+	--Внешние ключи
+	foreign key (id_abiturient) references Abiturient(aid) on update cascade,
+	foreign key (id_region) references Region(id) on update cascade,
+	foreign key (id_typeSettlement) references TypeSettlement(id) on update cascade	
+);
+
+--Индивидуальные достижения
+create table IndividualAchievement(
+	id int primary key,  --код
+	name text,			 --наименование
+	score int,           --балл
+	codeFIS text		 --код выгрузки в ФИС
+);
+
+--Абитуриент_Индивидуальные достижения 
+create table AbiturientAchievement(
+	nameOf_document text,    --название документа
+	series_document text,    --серия документа
+	number_document text,    --номер документа
+	issued_by text,          --кем выдан
+	dateOf_issue date,       --дата выдачи
+    id_abiturient int,       --код абитуриента
+	id_individualAchievement int, --код индивидуального достижения
+	
+	--Внешние ключи
+	foreign key (id_abiturient) references Abiturient(aid) on update cascade,
+	foreign key (id_individualAchievement) references IndividualAchievement(id) on update cascade
+);
+
+
